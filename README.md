9f4ffa4fd8d2921d8de5f8d800dffe1b2999cbb3
entpnrusman <entpnr.usman@gmail.com>
2023-06-08 11:12:40 +0500

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+[![‹Shoptegic/blog›](https://user-images.githubusercontent.com/37262772/213866538-70e7024e-64f5-4bd8-ba92-af803e6169e7.png)](https://shoptegic.vercel.app)  
+
+Blog built with Next.js 13, TypeScript, Contentlayer, Zustand and TailwindCSS.
+
+- 🔥 Using latest Next.js 13 features including the `/app` directory, SEO & Metadata, `next/font`, and React 18's Server Components
+- 🎛 Customizable reading experience - light/dark mode, serif/sans-serif, and font size
+- 🧩 MDX plugins and custom components
+- 💬 Comment sections using [Giscus](https://giscus.app/)
+- ⚡️ Vercel OG image generation at the Edge
+- 📖 Storybook v7 integration (published to [story.shop-tegic.com](https://story.shop-tegic.com))
---
+++ config/index.js
@@ -0,0 +1,67 @@
+// @ts-check
+
+/**
+ * The blog's configuration. Start here after cloning the repo.
+ * Hovering over the properties (in an editor like VSCode) will provide additional information about them.
+ */
+
+/** @type {import('./types').BlogConfig} */
+const blogConfig = {
+  url: 'https://shoptegic.vercel.app',
+  title: '‹shoptegic/blog›',
+  titleParts: ['shoptegic', 'blog'],
+  author: 'Muhammad Usman',
+  pages: {
+    home: {
+      title: 'Web Development Blog by Muhammad Usman',
+      description:
+        "My name is Usman and I'm a Full Stack Developer. Welcome to my dev blog! I write about Web Development and other topics I'm interested in.",
+    },
+    posts: {
+      url: '/posts',
+      title: 'Posts',
+      description:
+        "All my blog posts. I write about Web Development and other topics I'm interested in.",
+    },
+  },
+  theme: {
+    accentColor: {
+      light: '#be123c',
+      dark: '#fda4af',
+    },
+    codeBlockTheme: {
+      light: 'github-light',
+      dark: 'github-dark',
+    },
+  },
+  giscus: {
+    repo: 'kfirfitousi/blog',
+    repoId: 'R_kgDOIcM7JA',
+    category: 'Comments',
+    categoryId: 'DIC_kwDOIcM7JM4CTdK0',
+    mapping: 'title',
+    theme: {
+      light: 'light',
+      dark: 'dark_dimmed',
+    },
+  },
+  footerLinks: {
+    twitter: 'https://twitter.com/',
+    github: 'https://github.com/entpnrusman',
+    linkedin: 'https://www.linkedin.com/',
+    email: 'business@shop-tegic.com',
+    storybook: 'https://story.blog.kfirfitousi.com',
+    buyMeAPizza: 'https://buymeacoffee.com/kfirfitousi',
+  },
+  topics: [
+    'Web Development',
+    'React',
+    'TypeScript',
+    'Next.js',
+    'Rust',
+    'Design',
+    'Computer Vision',
+  ],
+};
+
+module.exports = { blogConfig };
